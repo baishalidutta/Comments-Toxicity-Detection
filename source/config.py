e53bb0d0e8d46a2804487706a907c967a5e200e4
Baishali Dutta <me@itsbaishali.com>
2021-02-15 10:07:51 +0100

    Moved Glove Word Embeddings

--- source/config.py
+++ source/config.py
@@ -7,13 +7,18 @@ __version__ = "0.1"
 #                               Configurations
 # -------------------------------------------------------------------------
 EMBEDDING_DIMENSION = 100
-EMBEDDING_FILE_LOC = '../glove/glove.6B.' + str(EMBEDDING_DIMENSION) + 'd.txt'
+EMBEDDING_FILE_LOC = '../model/glove/glove.6B.' + str(EMBEDDING_DIMENSION) + 'd.txt'
 TRAINING_DATA_LOC = '../data/train.csv'
 MAX_VOCAB_SIZE = 20000
 MAX_SEQUENCE_LENGTH = 100
 BATCH_SIZE = 128
 EPOCHS = 10
 VALIDATION_SPLIT = 0.2
-DETECTION_CLASSES = ['toxic', 'severe_toxic', 'obscene', 'threat',
-                     'insult', 'identity_hate']
+DETECTION_CLASSES = [
+    'toxic',
+    'severe_toxic',
+    'obscene',
+    'threat',
+    'insult',
+    'identity_hate']
 MODEL_LOC = '../model/comments_toxicity.h5'
