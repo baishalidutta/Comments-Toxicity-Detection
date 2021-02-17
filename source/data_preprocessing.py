e23ef8012057fd6c9a09ff2e0eac1458ea7ac5bd
Baishali Dutta <me@itsbaishali.com>
2021-02-17 03:48:29 +0100

    Refactored Data Preprocessing

--- source/data_preprocessing.py
+++ source/data_preprocessing.py
@@ -4,12 +4,9 @@ __license__ = "Apache License 2.0"
 __version__ = "0.1"
 
 # -------------------------------------------------------------------------
-#                           Importing the libraries
+#                           Importing Libraries
 # -------------------------------------------------------------------------
-import operator
-
 import numpy as np
-import pandas as pd
 from keras.layers import Embedding
 from keras.preprocessing.sequence import pad_sequences
 from keras.preprocessing.text import Tokenizer
@@ -20,18 +17,18 @@ from config import *
 # -------------------------------------------------------------------------
 #                           Data Preprocessing
 # -------------------------------------------------------------------------
-class DataPreprocess():
+class DataPreprocess:
     """
     Preprocess the data
     """
 
-    def __init__(self, data_location):
+    def __init__(self, data):
         """
         Initializes and prepares the data with necessary steps either to be trained
         or evaluated by the RNN model
-        :param data_location: the location of the data
+        :param data: the dataframe extracted from the .csv file
         """
-        self.data_location = data_location
+        self.data = data
 
         # The pre-trained word vectors used (http://nlp.stanford.edu/data/glove.6B.zip)
         word_to_vector = {}
@@ -48,12 +45,9 @@ class DataPreprocess():
         # print the total words found
         print(f'Total of {len(word_to_vector)} word vectors are found.')
 
-        # Import the training data
-        training_data = pd.read_csv(data_location)
-
         # Split the data into feature and target labels
-        comments = training_data['comment_text'].values
-        self.target_classes = training_data[DETECTION_CLASSES].values
+        comments = data['comment_text'].values
+        self.target_classes = data[DETECTION_CLASSES].values
 
         # Convert the comments (strings) into integers
         tokenizer = Tokenizer(num_words=MAX_VOCAB_SIZE)
@@ -87,164 +81,3 @@ class DataPreprocess():
                                          weights=[embedding_matrix],
                                          input_length=MAX_SEQUENCE_LENGTH,
                                          trainable=False)
-
-
-def build_vocab(texts):
-    sentences = texts.apply(lambda x: x.split()).values
-    vocab = {}
-    for sentence in sentences:
-        for word in sentence:
-            try:
-                vocab[word] += 1
-            except KeyError:
-                vocab[word] = 1
-    return vocab
-
-
-def check_coverage(vocab, embeddings_index):
-    known_words = {}
-    unknown_words = {}
-    nb_known_words = 0
-    nb_unknown_words = 0
-    for word in vocab.keys():
-        try:
-            known_words[word] = embeddings_index[word]
-            nb_known_words += vocab[word]
-        except:
-            unknown_words[word] = vocab[word]
-            nb_unknown_words += vocab[word]
-            pass
-
-    print('Found embeddings for {:.2%} of vocab'.format(len(known_words) / len(vocab)))
-    print('Found embeddings for  {:.2%} of all text'.format(nb_known_words / (nb_known_words + nb_unknown_words)))
-    unknown_words = sorted(unknown_words.items(), key=operator.itemgetter(1))[::-1]
-
-    return unknown_words
-
-
-# -------------------------------------------------------------------------
-#                           Processing Functions
-# -------------------------------------------------------------------------
-
-# Add words to embedding
-def add_lower_upper(embedding, vocab):
-    count = 0
-    for word in vocab:
-        if word.lower() in embedding and word not in embedding:
-            embedding[word] = embedding[word.lower()]
-            count += 1
-        if word in embedding and word.lower() not in embedding:
-            embedding[word.lower()] = embedding[word]
-            count += 1
-    print("Added %i words to embedding" % count)
-
-
-# Contraction mapping
-contraction_mapping = {"ain't": "is not", "aren't": "are not", "can't": "cannot", "'cause": "because",
-                       "could've": "could have", "couldn't": "could not",
-                       "didn't": "did not", "doesn't": "does not", "don't": "do not", "hadn't": "had not",
-                       "hasn't": "has not", "haven't": "have not", "he'd": "he would", "he'll": "he will",
-                       "he's": "he is", "how'd": "how did", "how'd'y": "how do you", "how'll": "how will",
-                       "how's": "how is", "I'd": "I would", "I'd've": "I would have", "I'll": "I will",
-                       "I'll've": "I will have", "I'm": "I am", "I've": "I have", "i'd": "i would",
-                       "i'd've": "i would have", "i'll": "i will", "i'll've": "i will have", "i'm": "i am",
-                       "i've": "i have", "isn't": "is not", "it'd": "it would", "it'd've": "it would have",
-                       "it'll": "it will", "it'll've": "it will have", "it's": "it is", "let's": "let us",
-                       "ma'am": "madam", "mayn't": "may not", "might've": "might have", "mightn't": "might not",
-                       "mightn't've": "might not have", "must've": "must have", "mustn't": "must not",
-                       "mustn't've": "must not have", "needn't": "need not", "needn't've": "need not have",
-                       "o'clock": "of the clock", "oughtn't": "ought not", "oughtn't've": "ought not have",
-                       "shan't": "shall not", "sha'n't": "shall not", "shan't've": "shall not have",
-                       "she'd": "she would", "she'd've": "she would have", "she'll": "she will",
-                       "she'll've": "she will have", "she's": "she is", "should've": "should have",
-                       "shouldn't": "should not", "shouldn't've": "should not have", "so've": "so have",
-                       "so's": "so as",
-                       "this's": "this is", "that'd": "that would", "that'd've": "that would have", "that's": "that is",
-                       "there'd": "there would", "there'd've": "there would have", "there's": "there is",
-                       "here's": "here is",
-                       "they'd": "they would", "they'd've": "they would have", "they'll": "they will",
-                       "they'll've": "they will have",
-                       "they're": "they are", "they've": "they have", "to've": "to have", "wasn't": "was not",
-                       "we'd": "we would", "we'd've": "we would have", "we'll": "we will", "we'll've": "we will have",
-                       "we're": "we are", "we've": "we have", "weren't": "were not", "what'll": "what will",
-                       "what'll've": "what will have",
-                       "what're": "what are", "what's": "what is", "what've": "what have", "when's": "when is",
-                       "when've": "when have",
-                       "where'd": "where did", "where's": "where is", "where've": "where have", "who'll": "who will",
-                       "who'll've": "who will have",
-                       "who's": "who is", "who've": "who have", "why's": "why is", "why've": "why have",
-                       "will've": "will have",
-                       "won't": "will not", "won't've": "will not have", "would've": "would have",
-                       "wouldn't": "would not",
-                       "wouldn't've": "would not have", "y'all": "you all", "y'all'd": "you all would",
-                       "y'all'd've": "you all would have",
-                       "y'all're": "you all are", "y'all've": "you all have", "you'd": "you would",
-                       "you'd've": "you would have", "you'll": "you will",
-                       "you'll've": "you will have", "you're": "you are", "you've": "you have"}
-
-
-def clean_contractions(text, mapping):
-    specials = ["’", "‘", "´", "`"]
-    for s in specials:
-        text = text.replace(s, "'")
-    text = ' '.join([mapping[t] if t in mapping else t for t in text.split(" ")])
-    return text
-
-
-# Clean special characters
-punct_mapping = {"‘": "'", "₹": "e", "´": "'", "°": "", "€": "euro", "™": "tm", "√": " square root ",
-                 "×": "x", "²": "2", "—": "-", "–": "-", "’": "'", "_": "-", "`": "'",
-                 '“': '"', '”': '"', "£": "pound", '∞': 'infinity', 'θ': 'theta',
-                 '÷': '/', 'α': 'alpha', '•': '.', 'à': 'a', '−': '-', 'β': 'beta',
-                 '∅': '', '³': '3', 'π': 'pi', '$': 'dollar'}
-
-puncts = "/-'?!.,#$%\'()*+-/:;<=>@[\\]^_`{|}~" + '""“”’' + '∞θ÷α•à−β∅³π‘₹´°£€\×™√²—–&'
-
-
-def clean_special_chars(text, punct, mapping):
-    for p in mapping:
-        text = text.replace(p, mapping[p])
-
-    for p in puncts:
-        text = text.replace(p, '')
-
-    specials = {'\u200b': ' ', '…': ' ... ', '\ufeff': '', 'करना': '',
-                'है': ''}  # Other special characters that I have to deal with in last
-    for s in specials:
-        text = text.replace(s, specials[s])
-
-    return text
-
-
-# Misspelled words and slangs
-mispell_dict = {'colour': 'color', 'centre': 'center', 'favourite': 'favorite', 'travelling': 'traveling',
-                'counselling': 'counseling',
-                'theatre': 'theater', 'cancelled': 'canceled', 'labour': 'labor', 'organisation': 'organization',
-                'wwii': 'world war 2', 'citicise': 'criticize', 'youtu ': 'youtube ', 'Qoura': 'Quora',
-                'sallary': 'salary',
-                'Whta': 'What', 'narcisist': 'narcissist', 'howdo': 'how do', 'whatare': 'what are',
-                'howcan': 'how can',
-                'howmuch': 'how much', 'howmany': 'how many', 'whydo': 'why do', 'doI': 'do I', 'theBest': 'the best',
-                'howdoes': 'how does', 'mastrubation': 'masturbation', 'mastrubate': 'masturbate',
-                "mastrubating": 'masturbating',
-                'pennis': 'penis', 'Etherium': 'Ethereum', 'narcissit': 'narcissist', 'bigdata': 'big data',
-                '2k17': '2017', '2k18': '2018',
-                'qouta': 'quota', 'exboyfriend': 'ex boyfriend', 'airhostess': 'air hostess', "whst": 'what',
-                'watsapp': 'whatsapp',
-                'demonitisation': 'demonetization', 'demonitization': 'demonetization',
-                'demonetisation': 'demonetization', ' ur ': 'your', ' u r ': 'you are'}
-
-
-def correct_spelling(text, dic):
-    for word in dic.keys():
-        text = text.replace(word, dic[word])
-    return text
-
-
-# General processing function
-def clean_text(text):
-    text = text.lower()
-    text = clean_contractions(text, contraction_mapping)
-    text = clean_special_chars(text, puncts, punct_mapping)
-    text = correct_spelling(text, mispell_dict)
-    return text
