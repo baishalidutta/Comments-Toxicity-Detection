93927833091d290f307f8a70af8b1cf8dd915d02
Baishali Dutta <me@itsbaishali.com>
2021-02-20 18:29:46 +0100

    Enhanced Model Training

--- source/model_training.py
+++ source/model_training.py
@@ -10,7 +10,8 @@ __version__ = "0.1"
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from keras.layers import Input, Dense, \
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+from keras.layers import Input, Dense, Dropout, \
     GlobalMaxPooling1D, LSTM, Bidirectional
 from keras.models import Model
 from sklearn.metrics import roc_auc_score
@@ -25,6 +26,7 @@ from data_preprocessing import DataPreprocess
 def build_rnn_model(data, target_classes, embedding_layer):
     """
     Build and Train the RNN architecture (Bidirectional LSTM)
+    :param data: the preprocessed padded data
     :param embedding_layer: Embedding layer comprising preprocessed comments
     :param target_classes: Assigned target labels for the comments
     :return: the trained model
@@ -32,8 +34,13 @@ def build_rnn_model(data, target_classes, embedding_layer):
     # create an LSTM network with a single LSTM
     input_ = Input(shape=(MAX_SEQUENCE_LENGTH,))
     x = embedding_layer(input_)
-    x = Bidirectional(LSTM(units=15, return_sequences=True))(x)
+    x = Bidirectional(LSTM(units=64,
+                           return_sequences=True,
+                           recurrent_dropout=0.2))(x)
     x = GlobalMaxPooling1D()(x)
+    x = Dense(units=64, activation='relu')(x)
+    x = Dropout(rate=0.2)(x)
+    # x = GlobalMaxPooling1D()(x)
 
     #  Sigmoid Classifier
     output = Dense(len(DETECTION_CLASSES), activation="sigmoid")(x)
@@ -48,15 +55,26 @@ def build_rnn_model(data, target_classes, embedding_layer):
                   optimizer='adam',
                   metrics=['accuracy'])
 
-    # Train the model
+    # Defining the callbacks
+    # TODO Check whether to use the restore_best_weights
+    early_stop = EarlyStopping(monitor='val_loss',
+                               patience=5,
+                               mode='min',
+                               restore_best_weights=True)
+
+    checkpoint = ModelCheckpoint(filepath=MODEL_LOC,  # saves the 'best' model
+                                 monitor='val_loss',
+                                 save_best_only=True,
+                                 mode='min')
+
+    # Fit the model
     history = model.fit(data,
                         target_classes,
                         batch_size=BATCH_SIZE,
                         epochs=EPOCHS,
-                        validation_split=VALIDATION_SPLIT)
-
-    # Save the model
-    model.save(MODEL_LOC)
+                        validation_split=VALIDATION_SPLIT,
+                        callbacks=[early_stop, checkpoint],
+                        verbose=1)
 
     # Return model training history
     return model, history
@@ -69,6 +87,7 @@ def plot_training_history(rnn_model, history, data, target_classes):
     """
     Generates plots for accuracy and loss
     :param rnn_model: the trained model
+    :param history: the model history
     :param data: preprocessed data
     :param target_classes: target classes for every comment
     :return: None
@@ -96,7 +115,7 @@ def plot_training_history(rnn_model, history, data, target_classes):
     # Print Average ROC_AUC_Score
     p = rnn_model.predict(data)
     aucs = []
-    for j in range(6):
+    for j in range(len(DETECTION_CLASSES)):
         auc = roc_auc_score(target_classes[:, j], p[:, j])
         aucs.append(auc)
     print(f'Average ROC_AUC Score: {np.mean(aucs)}')
@@ -108,6 +127,7 @@ def plot_training_history(rnn_model, history, data, target_classes):
 def execute():
     # Import the training data csv file and save it into a dataframe
     training_data = pd.read_csv(TRAINING_DATA_LOC)
+
     preprocessing = DataPreprocess(training_data)
     rnn_model, history = build_rnn_model(preprocessing.padded_data,
                                          preprocessing.target_classes,
