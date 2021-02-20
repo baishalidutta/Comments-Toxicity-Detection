4b3ed9d8d3436602d120f31e1bcac685662bdb39
Baishali Dutta <me@itsbaishali.com>
2021-02-20 18:29:34 +0100

    Refactored Model Evaluation

--- source/model_evaluation.py
+++ source/model_evaluation.py
@@ -15,41 +15,86 @@ from sklearn.metrics import roc_auc_score, accuracy_score
 from config import *
 from data_preprocessing import DataPreprocess
 
+
 # -------------------------------------------------------------------------
 #                   Prepare Test Data
 # -------------------------------------------------------------------------
-test_label = pd.read_csv('../data/test_labels.csv')
-test_comments = pd.read_csv('../data/test.csv')
+def prepare_test_data():
+    """
+    Loads the test dataset and prepare it for further evaluation
+    :return: prepared test data
+    """
+    test_label = pd.read_csv('../data/test_labels.csv')
+    test_comments = pd.read_csv('../data/test.csv')
 
-# Merge the two dataframes into one for better handling
-test_data = pd.merge(test_comments, test_label, on='id')
+    # Merge the two dataframes into one for better handling
+    test_data = pd.merge(test_comments, test_label, on='id')
 
-# Dropping the rows where correct label are not assigned
-# In such rows, the all labels are filled with -1
-test_data = test_data[test_data['toxic'] != -1]
+    # Dropping the rows where correct label are not assigned
+    # In such rows, the all labels are filled with -1
+    test_data = test_data[test_data['toxic'] != -1]
 
-# -------------------------------------------------------------------------
-#                   Preprocess Test Data
-# -------------------------------------------------------------------------
-preprocessing = DataPreprocess(test_data)
+    return test_data
+
 
 # -------------------------------------------------------------------------
 #                   Evaluate Preprocessed Test Data
 # -------------------------------------------------------------------------
-# load the trained CNN model
-rnn_model = load_model(MODEL_LOC)
-pred = rnn_model.predict(preprocessing.padded_data, steps=len(preprocessing.padded_data) / BATCH_SIZE, verbose=1)
+def make_prediction(preprocessing):
+    """
+    Loads the RNN model
+    :param preprocessing: prepared DataPreprocess instance
+    :return: the loaded model instance
+    """
+    rnn_model = load_model(MODEL_LOC)
 
-pred_binary = pred > 0.5
+    prediction = rnn_model.predict(preprocessing.padded_data,
+                                   steps=len(preprocessing.padded_data) / BATCH_SIZE,
+                                   verbose=1)
+    return prediction
 
-aucs = []
-for j in range(len(DETECTION_CLASSES)):
-    auc = roc_auc_score(preprocessing.target_classes[:, j], pred_binary[:, j])
-    aucs.append(auc)
-print(f'Average ROC_AUC Score on Test Data: {np.mean(aucs)}')
 
-accuracy = []
-for j in range(len(DETECTION_CLASSES)):
-    acc = accuracy_score(preprocessing.target_classes[:, j], pred_binary[:, j])
-    accuracy.append(acc)
-print(f'Average Accuracy Score on Test Data: {np.mean(accuracy)}')
+def evaluate_roc_auc(preprocessing, prediction_binary):
+    """
+    Evaluates the model
+    :param preprocessing: prepared DataPreprocess instance
+    :param prediction_binary: boolean expression for the predicted classes
+    """
+    aucs = []
+    for j in range(len(DETECTION_CLASSES)):
+        auc = roc_auc_score(preprocessing.target_classes[:, j], prediction_binary[:, j])
+        aucs.append(auc)
+
+    return np.mean(aucs)
+
+
+def evaluate_accuracy_score(preprocessing, prediction_binary):
+    """
+    Evaluates the accuracy score
+    :param preprocessing: prepared DataPreprocess instance
+    :param prediction_binary: boolean expression for the predicted classes
+    """
+    accuracy = []
+    for j in range(len(DETECTION_CLASSES)):
+        acc = accuracy_score(preprocessing.target_classes[:, j], prediction_binary[:, j])
+        accuracy.append(acc)
+
+    return np.mean(accuracy)
+
+
+# -------------------------------------------------------------------------
+#                               Main Execution
+# -------------------------------------------------------------------------
+def execute():
+    test_data = prepare_test_data()
+    preprocessing = DataPreprocess(test_data, do_load_existing_tokenizer=True)
+    prediction = make_prediction(preprocessing)
+    roc_auc = evaluate_roc_auc(preprocessing, prediction > 0.5)
+    accuracy = evaluate_accuracy_score(preprocessing, prediction > 0.5)
+
+    print(f'Average ROC_AUC Score on Test Data: {roc_auc}')
+    print(f'Average Accuracy Score on Test Data: {accuracy}')
+
+
+if __name__ == '__main__':
+    execute()
