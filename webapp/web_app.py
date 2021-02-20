787cfca05d5222471a0869b7bfbc7c9f7faf26e9
Baishali Dutta <me@itsbaishali.com>
2021-02-20 18:30:23 +0100

    Improved Web App

--- webapp/web_app.py
+++ webapp/web_app.py
@@ -4,32 +4,46 @@ __license__ = "Apache License 2.0"
 __version__ = "0.1"
 
 # -------------------------------------------------------------------------
-#                           Importing the libraries
+#                           Import Libraries
 # -------------------------------------------------------------------------
+import pickle
+
 import gradio as gr
 from keras.models import load_model
 from keras.preprocessing.sequence import pad_sequences
-from keras.preprocessing.text import Tokenizer
 
 from source.config import *
+from source.data_cleaning import clean_text
+
+# -------------------------------------------------------------------------
+#                     Load Existing Model and Tokenizer
+# -------------------------------------------------------------------------
 
 # load the trained model
 rnn_model = load_model(MODEL_LOC)
 
+# load the tokenizer
+with open(TOKENIZER_LOC, 'rb') as handle:
+    tokenizer = pickle.load(handle)
 
-def make_prediction(test_comment):
+
+# -------------------------------------------------------------------------
+#                           Main Application
+# -------------------------------------------------------------------------
+
+def make_prediction(input_comment):
     """
     Predicts the toxicity of the specified comment
+    :param input_comment: the comment to be verified
     """
-    test_comment = [test_comment]
-    tokenizer = Tokenizer(num_words=MAX_VOCAB_SIZE)
-    tokenizer.fit_on_texts(test_comment)
-    sequences = tokenizer.texts_to_sequences(test_comment)
+    input_comment = clean_text(input_comment)
+    input_comment = input_comment.split(" ")
+
+    sequences = tokenizer.texts_to_sequences(input_comment)
+    sequences = [[item for sublist in sequences for item in sublist]]
 
     padded_data = pad_sequences(sequences, maxlen=MAX_SEQUENCE_LENGTH)
-
     result = rnn_model.predict(padded_data, len(padded_data), verbose=1)
-    print(result)
 
     return \
         {
@@ -42,14 +56,14 @@ def make_prediction(test_comment):
         }
 
 
-input_comment = gr.inputs.Textbox(lines=17, placeholder="Enter your comment here")
+comment = gr.inputs.Textbox(lines=17, placeholder="Enter your comment here")
 
 title = "Comments Toxicity Detection"
 description = "This application uses a Bidirectional Long short-term memory (LSTM) Recurrent Neural Network (RNN) " \
               "model to predict whether a comment is classified as toxic in nature"
 
 gr.Interface(fn=make_prediction,
-             inputs=input_comment,
+             inputs=comment,
              outputs="label",
              title=title,
              description=description) \
