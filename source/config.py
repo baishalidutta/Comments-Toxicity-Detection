91399b737356509cdc533985e1797e2c2da37853
Baishali Dutta <me@itsbaishali.com>
2022-01-15 20:03:55 +0100

    Updated Copyrights

--- source/config.py
+++ source/config.py
@@ -1,5 +1,5 @@
 __author__ = "Baishali Dutta"
-__copyright__ = "Copyright (C) 2021 Baishali Dutta"
+__copyright__ = "Copyright (C) 2022 Baishali Dutta"
 __license__ = "Apache License 2.0"
 __version__ = "0.1"
 
